26d27ed1c35b2b843de28b2675ef8552c0291f88
cmes31419 <cmes31419@gmail.com>
2022-08-30 12:13:42 +0800

    fix pC validator

--- pC/validator/validator-2.cpp
+++ pC/validator/validator-2.cpp
@@ -7,15 +7,32 @@ int main() {
 	registerValidation();
 
 	int N = inf.readInt(-1000000000, 1000000000);
+	inf.readSpace();
+	int M = inf.readInt(-1000000000, 1000000000);
+	inf.readSpace();
+	int T = inf.readInt(-1000000000, 1000000000);
 	inf.readEoln();
+	ensure(0<N&&N<=100);
+	ensure(0<M&&M<=100);
+	ensure(0<M&&M<=1000);
 	
-	ensure(0<N&&N<=10000);
-	  
-	for(int i=0;i<N;i++){
-		int ai = inf.readInt(-1000000000, 1000000000);
-		inf.readSpace();
-	}
+	int x = inf.readInt(-1000000000, 1000000000);
+	inf.readSpace();
+	int y = inf.readInt(-1000000000, 1000000000);
 	inf.readEoln();
+	ensure(0<=x&&x<N);
+	ensure(0<=y&&y<M);
+
+	for(int i=1;i<T;i++){
+		int xi = inf.readInt(-1000000000, 1000000000);
+		inf.readSpace();
+		int yi = inf.readInt(-1000000000, 1000000000);
+		inf.readSpace();
+		int wi = inf.readInt(-1000000000, 1000000000);
+		inf.readEoln();
+		ensure(0<=xi&&xi<N);
+		ensure(0<=yi&&yi<M);
+	}
 
 	inf.readEof();
 	
